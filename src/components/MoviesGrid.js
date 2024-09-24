ec57482d17ee3a057d76e7b4b3881161f1cdc6a5
Bruno Sanchez <bruno.sanchez.parra@gmail.com>
2024-09-24 11:41:53 -0700

    Added text filtering by Movie Titles

--- src/components/MoviesGrid.js
+++ src/components/MoviesGrid.js
@@ -9,24 +9,45 @@ export default function MoviesGrid(){
     // State to store the movies
     const [movies, setMovies] = useState([]);
 
+    // State to store the search term
+    const [searchTerm, setSearchTerm] = useState("");
+
     useEffect(() => {
         fetch("movies.JSON")
         .then(response => response.json())
         .then(data => setMovies(data))
-
     }, []);
 
+    // The handleSearchChange function is created to handle the search input change
+    const handleSearchChange = (e) => {
+        setSearchTerm(e.target.value);
+    };
+
+    // Takes the movies array and filters it based on the search term
+    const filteredMovies = movies.filter(movie => 
+        movie.title.toLowerCase().includes(searchTerm.toLowerCase())
+    )
+
     return (
-        <div className='movies-grid'>
-            {
-                // Loop through the movies array and create a div for each movie
-                // The key attribute is required when creating a list of elements
-            
-                movies.map(movie => (
-                    // The MovieCard Component is used to display the movie details
-                    <MovieCard movie={movie} key={movie.id}></MovieCard>
-                ))
-            }
+      <div>
+        <input 
+            className="search-input"
+            type="text"
+            placeholder="Search movies..."
+            value={searchTerm}
+            onChange={handleSearchChange}
+        />
+        <div className="movies-grid">
+          {
+            // Loop through the movies array and create a div for each movie
+            // The key attribute is required when creating a list of elements
+
+            filteredMovies.map((movie) => (
+              // The MovieCard Component is used to display the movie details
+              <MovieCard movie={movie} key={movie.id}></MovieCard>
+            ))
+          }
         </div>
+      </div>
     );
 }
