98192ff1ba7445566f7dc2b244fcbb773fb23eb3
Bruno Sanchez <bruno.sanchez.parra@gmail.com>
2024-09-24 20:13:01 -0700

    Added filtering using event handling to get movies that match criteria

--- src/components/MoviesGrid.js
+++ src/components/MoviesGrid.js
@@ -12,6 +12,12 @@ export default function MoviesGrid(){
     // State to store the search term
     const [searchTerm, setSearchTerm] = useState("");
 
+    // State to store the genre
+    const [genre, setGenre] = useState("All Genres");
+
+    // State to store the rating
+    const [rating, setRating] = useState("All");
+
     useEffect(() => {
         fetch("movies.JSON")
         .then(response => response.json())
@@ -22,11 +28,47 @@ export default function MoviesGrid(){
     const handleSearchChange = (e) => {
         setSearchTerm(e.target.value);
     };
+    // Filters the movies based on the rating
+    const handleRatingChange = (e) => {
+        setRating(e.target.value);
+    };
+
+    // Filters the movies based on the genre
+    const handleGenreChange = (e) => {
+        setGenre(e.target.value);
+    };
+
+    // Checks if the movie matches the genre
+    const matchesGenre = (movie, genre) => {
+      return genre === "All Genres"|| movie.genre.toLowerCase() === genre.toLowerCase();
+    }
+
+    // Checks if the movie matches the search term
+    const matchesSearchTerm = (movie, searchTerm) => {
+      return movie.title.toLowerCase().includes(searchTerm.toLowerCase());
+    }
+
+    const matchesRating = (movie, rating) => {
+      switch(rating){
+        case "All":
+          return true;
+        case "Good":
+          return movie.rating >= 8;
+        case "Ok":
+          return movie.rating >= 5 && movie.rating < 8;
+        case "Bad":
+          return movie.rating < 5;
+        default:
+          return
+      }
+    }
 
     // Takes the movies array and filters it based on the search term
-    const filteredMovies = movies.filter(movie => 
-        movie.title.toLowerCase().includes(searchTerm.toLowerCase())
-    )
+    const filteredMovies = movies.filter((movie) => 
+        matchesGenre(movie, genre) && 
+        matchesRating(movie, rating) &&
+        matchesSearchTerm(movie, searchTerm)
+    );
 
     return (
       <div>
@@ -37,6 +79,47 @@ export default function MoviesGrid(){
             value={searchTerm}
             onChange={handleSearchChange}
         />
+
+        <div className="filter-bar">
+          <div className="filter-slot">
+            <label>Genre</label>
+            <select className="filter-dropdown" value={genre} onChange={handleGenreChange}>
+              <option>
+                All Genres
+              </option>
+              <option>
+                Action
+              </option>
+              <option>
+                Drama
+              </option>
+              <option>
+                Fantasy
+              </option>
+              <option>
+                Horror
+              </option>
+            </select>
+          </div>
+          <div className="filter-slot">
+            <label>Rating</label>
+            <select className="filter-dropdown" value={rating} onChange={handleRatingChange}>
+              <option>
+                All
+              </option>
+              <option>
+                Good
+              </option>
+              <option>
+                Ok
+              </option>
+              <option>
+                Bad
+              </option>
+            </select>
+          </div>
+        </div>
+
         <div className="movies-grid">
           {
             // Loop through the movies array and create a div for each movie
